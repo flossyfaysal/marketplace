780f351fd587f4b571ef39f3772eab89629e58ed
Sheikh Faysal <46521037+flossyfaysal@users.noreply.github.com>
2021-08-01 21:39:44 +0600

    Master Polluted

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ users.push({name: "faysal", age: 27, isAlive: true});
 const getLastPop = users.pop();
 const getLastPush = users.push();
 
-const output = [getLastPop, getLastPush];
+const output = [getLastPop, getLastPush, "Now Master Ahed and confliced"];
 
 const component = output.push(output);
